c921573d2d57a7c248d42eba676248bc90a11b26
gromi1985 <alpauta73@gmail.com>
2022-01-31 22:53:10 +0100

    Nivel III Terminado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 /**Variables para la peticion asincronica JOKE*/
 var API_URL_JOKE_0 = "https://icanhazdadjoke.com/";
-// const API_URL_JOKE_1: string = "https://api.chucknorris.io/jokes/random";
 var API_URL_JOKE_1 = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random";
-var API_OPCIONES_PETICION_JOKE_0 = {
+var API_CONF_REQUEST_TEMPLATE = {
     headers: {
         Accept: "application/json"
     }
@@ -15,12 +14,12 @@ var API_OPCIONES_PETICION_JOKE_1 = {
         "x-rapidapi-key": "fa472c5b6fmsh3519c15a39db3b9p1f47b8jsnf07e2c639a4a"
     }
 };
-var data_confJoke1 = {};
 /**Parametros para la peticion asincronica WEATHER*/
 var data_confWeather = {
     id: "6356131",
     appid: "3f12566373dfcec257a5ff896a50556e",
-    lang: "es"
+    lang: "es",
+    units: "metric"
 };
 /** serializo un objeto en una lista de parámetros de consulta de URL */
 var API_URL_WEATHER = "http://api.openweathermap.org/data/2.5/weather?";
@@ -28,12 +27,6 @@ var paramsURL = new URLSearchParams(data_confWeather).toString();
 var apiUrlWeatherFinal = API_URL_WEATHER.concat(paramsURL);
 //Bloqueamos los parametros de configuracion para que no sea modificable por consola.
 Object.freeze(data_confWeather);
-// const API_URL_WEATHER = `http://api.openweathermap.org/data/2.5/weather?id=${data_conf.idCity}&appid=${data_conf.appid}&lang=${data_conf.lang} `;
-var API_OPCIONES_PETICION_WEATHER = {
-    headers: {
-        Accept: "application/json"
-    }
-};
 /**Array de objetos de tipo interfaceUnitJoke y al ser un const debo inicializarla */
 var reportAcudits = [];
 var jokeGeneral = "";
@@ -84,6 +77,57 @@ var seleccionJoke = function (option) {
         default:
     }
 };
+var iconItemWeather = function (id) {
+    var valueId = 0;
+    var dataImg;
+    if (id > 199 && id < 299)
+        valueId = 1;
+    else if (id > 299 && id < 399)
+        valueId = 2;
+    else if (id > 499 && id < 505)
+        valueId = 3;
+    else if (id == 511)
+        valueId = 4;
+    else if (id > 519 && id < 532)
+        valueId = 5;
+    else if (id > 599 && id < 700)
+        valueId = 6;
+    else if (id > 700 && id < 800)
+        valueId = 7;
+    else if (id == 800)
+        valueId = 8;
+    else if (id > 800)
+        valueId = 9;
+    switch (valueId) {
+        case 1:
+            dataImg = 'thunder.svg';
+            break;
+        case 2:
+            dataImg = 'rainy-4.svg';
+            break;
+        case 3:
+            dataImg = 'rainy-3.svg';
+            break;
+        case 4:
+            dataImg = 'snowy-5.svg';
+            break;
+        case 5:
+            dataImg = 'rainy-6.svg';
+            break;
+        case 6:
+            dataImg = 'snowy-6.svg';
+            break;
+        case 8:
+            dataImg = 'day.svg';
+            break;
+        case 9:
+            dataImg = 'cloudy.svg';
+            break;
+        default:
+            dataImg = 'day.svg';
+    }
+    return dataImg;
+};
 /******************************JOKE **********************************/
 if (button) {
     button.addEventListener("click", function () {
@@ -92,7 +136,7 @@ if (button) {
 }
 var jokeObtain_0 = function () {
     if (button && HTMLResponseJoke && buttonFace) {
-        var myFirstPromise_A = fetch(API_URL_JOKE_0, API_OPCIONES_PETICION_JOKE_0)
+        var myFirstPromise_A = fetch(API_URL_JOKE_0, API_CONF_REQUEST_TEMPLATE)
             .then(function (response) { return response.json(); })
             .then(function (data) {
             if (data.status == 200) {
@@ -134,22 +178,33 @@ var jokeObtain_1 = function () {
 };
 //Opto por indicarle a la promesa el formato de los datos que me devuelcva auqnue en este caso por defecto esta API devuelve los datos en formato de json, por lo que realmente no seria necesario.
 /******************************WEATHER **********************************/
-if (HTMLResponseWeather) {
-    var myFirstPromise_B = fetch(apiUrlWeatherFinal, API_OPCIONES_PETICION_WEATHER)
-        .then(function (response) { return response.json(); })
-        .then(function (data) {
-        if (data.cod == 200) {
-            var cleanElement = HTMLResponseWeather.querySelector("p:first-child");
-            if (cleanElement)
-                cleanElement.remove();
-            var newWeather = document.createElement("p");
-            newWeather.classList.add("dataWeather");
-            newWeather.innerHTML = data.weather[0].description;
-            HTMLResponseWeather.append(newWeather);
-            weatherGeneral = data.weather;
-        }
-    });
-    myFirstPromise_B["catch"](function (error) {
-        return console.log("Error en la promesa: ", error);
-    });
-}
+window.addEventListener('load', function () {
+    if (HTMLResponseWeather) {
+        var myFirstPromise_B = fetch(apiUrlWeatherFinal, API_CONF_REQUEST_TEMPLATE)
+            .then(function (response) { return response.json(); })
+            .then(function (data) {
+            if (data.cod == 200) {
+                var cleanElement = HTMLResponseWeather.querySelectorAll("p");
+                if (cleanElement)
+                    cleanElement.forEach(function (e) { return e.remove(); });
+                var newWeatherTemp = document.createElement("p");
+                newWeatherTemp.classList.add("dataWeather-temp");
+                newWeatherTemp.innerHTML = "".concat(Math.round(+data.main.temp), " \u00B0C");
+                var newWeatherImg = document.createElement("img");
+                newWeatherImg.classList.add("dataWeather-img");
+                newWeatherImg.src = "../img/animated/".concat(iconItemWeather(data.weather[0].id));
+                // newWeatherImg.innerHTML = data.weather[0].description;  
+                /* let newWeatherCity = document.createElement("p");
+                 newWeatherCity.classList.add("dataWeather-city");
+                 newWeatherCity.innerHTML = data.name;   */
+                HTMLResponseWeather.append(newWeatherImg);
+                HTMLResponseWeather.append(newWeatherTemp);
+                // HTMLResponseWeather.append(newWeatherCity);
+                weatherGeneral = data.weather;
+            }
+        });
+        myFirstPromise_B["catch"](function (error) {
+            return console.log("Error en la promesa: ", error);
+        });
+    }
+});
